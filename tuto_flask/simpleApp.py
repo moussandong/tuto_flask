a98c82101ce28994255b779389246cc3dea79144
moussa ndong <moussabaye94@egmail.com>
2021-12-15 15:56:01 +0100

    sending form data with flaskApp

--- tuto_flask/simpleApp.py
+++ tuto_flask/simpleApp.py
@@ -1,33 +1,18 @@
-# Importing flask module in the project is mandatory
-# An object of Flask class is our WSGI application.
-from flask import Flask
-
-# Flask constructor takes the name of
-# current module (__name__) as argument.
+from flask import Flask, redirect, url_for, request
 app = Flask(__name__)
 
-# The route() function of the Flask class is a decorator,
-# which tells the application which URL should call
-# the associated function.
-@app.route('/')
-# ‘/’ URL is bound with hello_world() function.
-def hello_world():
-	return 'Hello World'
+@app.route('/success/<name>')
+def success(name):
+	return 'welcome %s' % name
 
-def gfg():
-   return 'geeksforgeeks'
-app.add_url_rule('/gfg', 'g2g', gfg)
+@app.route('/login',methods = ['POST', 'GET'])
+def login():
+	if request.method == 'POST':
+		user = request.form['nm']
+		return redirect(url_for('success',name = user))
+	else:
+		user = request.args.get('nm')
+		return redirect(url_for('success',name = user))
 
-@app.route('/hello/<name>')
-def hello_name(name):
-   return 'Hello %s!' % name
-
-
-
-
-# main driver function
 if __name__ == '__main__':
-
-	# run() method of Flask class runs the application
-	# on the local development server.
-	app.run()
+	app.run(debug = True)
